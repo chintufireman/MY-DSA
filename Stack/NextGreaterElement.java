eb9f14a32527618e6d0f4414464e7339fe233f40
Harshwardhan <88778118+chintufireman@users.noreply.github.com>
2021-09-01 16:46:20 +0530

    [V.V.VIMP]next greater element in array (MEDIUM)
    program for next greater element in array using stack

---
+++ Stack/NextGreaterElement.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+ 
+class NextGreaterElement {
+ 
+    static int arr[] = { 11, 13, 21, 3 };
+ 
+    
+    public static void printNGE()
+    {
+        Stack<Integer> s = new Stack<>();
+        int nge[] = new int[arr.length];
+ 
+        for (int i = arr.length - 1; i >= 0; i--)
+        {
+            
+            if (!s.empty())
+            {
+                while (!s.empty()
+                       && s.peek() <= arr[i])
+                {
+                    s.pop();
+                }
+            }
+            nge[i] = s.empty() ? -1 : s.peek();
+            s.push(arr[i]);
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i] +
+                               " --> " + nge[i]);
+    }
+ 
+    /* Driver Code */
+    public static void main(String[] args)
+    {
+      
+        printNGE();
+    }
+}
